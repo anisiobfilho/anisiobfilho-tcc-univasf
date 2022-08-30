e72564c38961d502fecb0b81fe3cd6f8c143c28b
Anísio Filho <anisiobfilho@gmail.com>
2022-08-30 09:06:15 -0300

    Atualiza

--- pages/Usagem_dos_Modelos.py
+++ pages/Usagem_dos_Modelos.py
@@ -82,39 +82,35 @@ nltk.download('punkt')
 stopWords = nltk.corpus.stopwords.words('portuguese')
 
 
-@st.cache()
-def gera_ekprhasis():
-    text_processor = TextPreProcessor(
-        # terms that will be normalized
-        normalize=['url', 'email', 'percent', 'money', 'phone', 'user',
-            'time', 'url', 'date', 'number'],
-        # terms that will be annotated
-        annotate={'hashtag', 'allcaps', 'elongated', 'repeated',
-            'emphasis', 'censored'},
-        fix_html=True,  # fix HTML tokens
+text_processor = TextPreProcessor(
+    # terms that will be normalized
+    normalize=['url', 'email', 'percent', 'money', 'phone', 'user',
+        'time', 'url', 'date', 'number'],
+    # terms that will be annotated
+    annotate={'hashtag', 'allcaps', 'elongated', 'repeated',
+        'emphasis', 'censored'},
+    fix_html=True,  # fix HTML tokens
 
-        # corpus from which the word statistics are going to be used 
-        # for word segmentation 
-        segmenter='twitter', 
+    # corpus from which the word statistics are going to be used 
+    # for word segmentation 
+    segmenter='twitter', 
 
-        # corpus from which the word statistics are going to be used 
-        # for spell correction
-        corrector='twitter', 
+    # corpus from which the word statistics are going to be used 
+    # for spell correction
+    corrector='twitter', 
 
-        unpack_hashtags=True,  # perform word segmentation on hashtags
-        unpack_contractions=True,  # Unpack contractions (can't -> can not)
-        spell_correct_elong=False,  # spell correction for elongated words
+    unpack_hashtags=True,  # perform word segmentation on hashtags
+    unpack_contractions=True,  # Unpack contractions (can't -> can not)
+    spell_correct_elong=False,  # spell correction for elongated words
 
-        # select a tokenizer. You can use SocialTokenizer, or pass your own
-        # the tokenizer, should take as input a string and return a list of tokens
-        tokenizer=SocialTokenizer(lowercase=True).tokenize,
+    # select a tokenizer. You can use SocialTokenizer, or pass your own
+    # the tokenizer, should take as input a string and return a list of tokens
+    tokenizer=SocialTokenizer(lowercase=True).tokenize,
 
-        # list of dictionaries, for replacing tokens extracted from the text,
-        # with other expressions. You can pass more than one dictionaries.
-        dicts=[emoticons]
-        )
-    return text_processor
-text_processor = gera_ekprhasis()
+    # list of dictionaries, for replacing tokens extracted from the text,
+    # with other expressions. You can pass more than one dictionaries.
+    dicts=[emoticons]
+    )
 
 @st.cache()
 def carrega_modelo_word2vec(path):
